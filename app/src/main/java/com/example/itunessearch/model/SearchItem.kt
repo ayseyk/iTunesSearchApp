45a46535e2ac4488a83895fc1156df65076082c2
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2022-08-03 09:35:53 +0300

    Create model

--- app/src/main/java/com/example/itunessearch/model/SearchItem.kt
+++ app/src/main/java/com/example/itunessearch/model/SearchItem.kt
@@ -1,9 +1,7 @@
 package com.example.itunessearch.model
 
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 data class SearchItem(
     @SerializedName("artworkUrl100")
     val imageUrl: String?,
