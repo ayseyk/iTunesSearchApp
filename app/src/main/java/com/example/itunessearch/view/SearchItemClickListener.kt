1f17ad54863159cc8bbc6fb6df44f7f8395c2fc1
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2022-08-04 16:14:34 +0300

    Create Adapter and fill the list

---
+++ app/src/main/java/com/example/itunessearch/view/SearchItemClickListener.kt
@@ -0,0 +1,8 @@
+package com.example.itunessearch.view
+
+import android.view.View
+
+interface SearchItemClickListener {
+    fun onClick(v : View){
+    }
+}
---
+++ app/src/main/java/com/example/itunessearch/view/SearchItemListAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.itunessearch.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.itunessearch.R
+import com.example.itunessearch.model.SearchItem
+import kotlinx.android.synthetic.main.fragment_search.view.*
+import kotlinx.android.synthetic.main.item_search.view.*
+
+class SearchItemListAdapter(private val searchItemList: ArrayList<SearchItem>) : RecyclerView
+.Adapter<SearchItemListAdapter.SearchItemViewHolder>(), SearchItemClickListener {
+
+    class SearchItemViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+
+    fun updateSearchItemList(newSearchItemList: List<SearchItem>) {
+        searchItemList.clear()
+        searchItemList.addAll(newSearchItemList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_search, parent, false)
+        return SearchItemViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = searchItemList.size
+
+    override fun onBindViewHolder(holder: SearchItemViewHolder, position: Int) {
+        //holder.view.itemImage = searchItemList[position].imageUrl
+        holder.apply {
+            view.itemName.text = searchItemList[position].name
+            view.itemPrice.text = searchItemList[position].price.toString()
+            view.releaseDate.text = searchItemList[position].releaseDate
+        }
+        /*holder.view.searchItemsList.setOnClickListener {
+            onClick(holder.view)
+        }*/
+    }
+
+    override fun onClick(v: View) {
+        for (item in searchItemList) {
+            if (v.tag == item.name) {
+                val action = SearchFragmentDirections.searchToDetail(item)
+                Navigation.findNavController(v).navigate(action)
+            }
+        }
+    }
+}
