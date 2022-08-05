02f7177cc847ef189c8e9eac41042f242dc7b0b0
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2022-08-05 08:49:13 +0300

    Get image with glide

--- app/src/main/java/com/example/itunessearch/util/Util.kt
+++ app/src/main/java/com/example/itunessearch/util/Util.kt
@@ -1,7 +1,22 @@
 package com.example.itunessearch.util
 
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.itunessearch.R
+
 class Util {
     companion object {
         const val BASE_URL = "https://itunes.apple.com/"
     }
 }
+
+fun ImageView.loadImage(uri: String?) {
+    val options = RequestOptions()
+        .error(R.drawable.image_not_found)
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(uri)
+        .into(this)
+}
+
--- app/src/main/java/com/example/itunessearch/view/SearchItemListAdapter.kt
+++ app/src/main/java/com/example/itunessearch/view/SearchItemListAdapter.kt
@@ -1,14 +1,22 @@
 package com.example.itunessearch.view
 
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.example.itunessearch.R
 import com.example.itunessearch.model.SearchItem
+import com.example.itunessearch.util.loadImage
 import kotlinx.android.synthetic.main.fragment_search.view.*
 import kotlinx.android.synthetic.main.item_search.view.*
+import kotlinx.coroutines.NonDisposableHandle.parent
+import kotlin.coroutines.coroutineContext
+
 
 class SearchItemListAdapter(private val searchItemList: ArrayList<SearchItem>) : RecyclerView
 .Adapter<SearchItemListAdapter.SearchItemViewHolder>(), SearchItemClickListener {
@@ -29,7 +37,9 @@ class SearchItemListAdapter(private val searchItemList: ArrayList<SearchItem>) :
     override fun getItemCount(): Int = searchItemList.size
 
     override fun onBindViewHolder(holder: SearchItemViewHolder, position: Int) {
-        //holder.view.itemImage = searchItemList[position].imageUrl
+
+        holder.view.itemImage.loadImage(searchItemList[position].imageUrl)
+
         holder.apply {
             view.itemName.text = searchItemList[position].name
             view.itemPrice.text = searchItemList[position].price.toString()
