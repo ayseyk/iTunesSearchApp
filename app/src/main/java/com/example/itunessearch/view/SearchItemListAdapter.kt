d96b26980a629a46650a35f057c1b72978676692
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2022-08-06 13:59:14 +0300

    Add dataBinding for search screen

--- app/src/main/java/com/example/itunessearch/view/SearchItemListAdapter.kt
+++ app/src/main/java/com/example/itunessearch/view/SearchItemListAdapter.kt
@@ -3,19 +3,18 @@ package com.example.itunessearch.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.itunessearch.R
+import com.example.itunessearch.databinding.ItemSearchBinding
 import com.example.itunessearch.model.SearchItem
 import com.example.itunessearch.util.getDate
-import com.example.itunessearch.util.loadImage
-import kotlinx.android.synthetic.main.item_search.view.*
-
 
 class SearchItemListAdapter(private val searchItemList: ArrayList<SearchItem>) : RecyclerView
 .Adapter<SearchItemListAdapter.SearchItemViewHolder>(), SearchItemClickListener {
 
-    class SearchItemViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+    class SearchItemViewHolder(var view: ItemSearchBinding) : RecyclerView.ViewHolder(view.root)
 
     fun updateSearchItemList(newSearchItemList: List<SearchItem>) {
         searchItemList.clear()
@@ -29,29 +28,21 @@ class SearchItemListAdapter(private val searchItemList: ArrayList<SearchItem>) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_search, parent, false)
+        val inflater = LayoutInflater.from(parent.context)
+        val view = DataBindingUtil.inflate<ItemSearchBinding>(inflater, R.layout.item_search, parent, false)
         return SearchItemViewHolder(view)
     }
 
     override fun getItemCount(): Int = searchItemList.size
 
     override fun onBindViewHolder(holder: SearchItemViewHolder, position: Int) {
-        holder.apply {
-            view.itemImage.loadImage(searchItemList[position].imageUrl)
-            view.itemName.text = searchItemList[position].name
-            view.releaseDate.text = getDate(searchItemList[position], holder.view)
+        holder.view.searchItem = searchItemList[position]
+        holder.view.listener = this
+        holder.view.releaseDate.text = getDate(searchItemList[position], holder.itemView)
 
-            if (searchItemList[position].price != null) {
-                view.itemPrice.text = holder.view.resources.getString(
-                    R.string.price_format,
-                    searchItemList[position].price.toString()
-                )
-                view.itemPrice.visibility = View.VISIBLE
-            }
-        }
         holder.view.searchItemLayout.setOnClickListener {
             val action = SearchFragmentDirections.searchToDetail(searchItemList[position])
-            Navigation.findNavController(holder.view).navigate(action)
+            Navigation.findNavController(it).navigate(action)
         }
     }
 
