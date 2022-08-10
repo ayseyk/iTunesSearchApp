34f42c5cffafdf1f58c0ce020e3dff017fa53965
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2022-08-10 08:05:11 +0300

    fix and update project

--- app/src/main/java/com/example/itunessearch/util/Util.kt
+++ app/src/main/java/com/example/itunessearch/util/Util.kt
@@ -1,16 +1,12 @@
 package com.example.itunessearch.util
 
-import android.os.Build
 import android.view.View
 import android.widget.ImageView
-import androidx.annotation.RequiresApi
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.itunessearch.R
 import com.example.itunessearch.model.SearchItem
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 class Util {
     companion object {
@@ -25,7 +21,7 @@ fun ImageView.loadImage(uri: String?) {
         .setDefaultRequestOptions(options)
         .load(uri)
         .into(this)
-}//"1959-07-27T07:00:00Z"
+}
 
 fun getDate(searchItem: SearchItem, view: View): String {
     val dateList = searchItem.releaseDate?.split("-", "T")
@@ -34,13 +30,15 @@ fun getDate(searchItem: SearchItem, view: View): String {
         dateList?.get(2), dateList?.get(1), dateList?.get(0)
     )
 }
-
-@RequiresApi(Build.VERSION_CODES.O)
-private fun getFormattedDate(date: String?) = LocalDateTime.parse(
-    date, DateTimeFormatter.ISO_DATE_TIME
-).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
-
 @BindingAdapter("android:imageUrl")
 fun loadImageFromUrl(view: ImageView, url: String?) {
     view.loadImage(url)
 }
+
+//"1959-07-27T07:00:00Z"
+/*
+@RequiresApi(Build.VERSION_CODES.O) //alternative date formatter fun
+fun getFormattedDate(view: TextView, date: String?) = LocalDateTime.parse(
+    date, DateTimeFormatter.ISO_DATE_TIME
+).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+*/
